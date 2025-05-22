9a30b06ae586e65af42896edafe681cf0d8dca39
Perspect <loic29.urien@gmail.com>
2025-05-22 13:41:37 +0000

    11 fait

--- sujet 11/25-NSI-11.py
+++ sujet 11/25-NSI-11.py
@@ -50,7 +50,6 @@ def somme_max(tab):
     for i in range(1, n):
         if sommes_max[i] > sommes_max[maximum] : 
             maximum = i
-
     return sommes_max[maximum] 
 
 
@@ -63,7 +62,7 @@ def somme_max(tab):
     sommes_max[0] = tab[0]
     # on calcule la plus grande somme se terminant en i
     for i in range(1,n):
-        if sommes_max[i-1] + tab[i] > sommes_max[i-1]: 
+        if sommes_max[i-1] + tab[i] > tab[i]: 
             sommes_max[i] = sommes_max[i-1] + tab[i]
         else:
             sommes_max[i] = tab[i]
